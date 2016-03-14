f23528695e8a9b25742dc49dbb98a7f6bfe52512
Asier Tarancón García <asiertarancon@outlook.com>
2016-03-14 21:06:30 +0100

    Updated filter median

--- atg.ImagingUWP/atg.ImagingUWP/Effects/MedianFilter.cs
+++ atg.ImagingUWP/atg.ImagingUWP/Effects/MedianFilter.cs
@@ -49,44 +49,105 @@ namespace atg.ImagingUWP.Effects
             public BlockBasedWorker()
                 : base(new[] { ColorMode.Bgra8888 }) // Supported color modes.
             {
+                
             }
 
             protected override void OnProcess(PixelRegion sourcePixelRegion, PixelRegion targetPixelRegion)
             {
+                SortedSet<uint> array = new SortedSet<uint>();
 
+                //int width = Convert.ToInt32(sourcePixelRegion.ImageSize.Width);                
+                
                 targetPixelRegion.ForEachRow((index, width, position) =>
                 {
-                    for (int i = 0; i < width; ++i)
+                    for (int i = 0; i < width; i++)
                     {
-                        //Almacenamos los pixels alrededor del actual en un array ordenado para coger la mediana
-                        //var c = sourcePixelRegion.ImagePixels[index + i];
-                        List<uint> array = new List<uint>();
-
                         var pixel = index + i;
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width - 1));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width + 1));
-                                                                      
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - 1));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + 1));
-                                                                      
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width - 1));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width));
-                        array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width + 1));
-                        
-                        targetPixelRegion.ImagePixels[pixel] = array.OrderBy(it=>it).ToList()[4];
-                    }
+                        array.Clear();
+                        //var pixel = Convert.ToInt32(index + position.X + i);
 
+                        var pixelCentral = GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel, 0);
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel - width - 1, pixelCentral));
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel - width, pixelCentral));
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel - width + 1, pixelCentral));
+
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel - 1, pixelCentral));
+                        array.Add(pixelCentral);
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel + 1, pixelCentral));
+
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel + width - 1, pixelCentral));
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel + width, pixelCentral));
+                        array.Add(GetPixelOrDefault(sourcePixelRegion.ImagePixels, pixel + width + 1, pixelCentral));
+
+                        targetPixelRegion.ImagePixels[pixel] = sourcePixelRegion.ImagePixels[pixel];// array.ElementAt((array.Count- 1) /2);//.OrderBy(it => it).ToList()[4];
+                    }
                 });
+                //for (; pixel<sourcePixelRegion.ImagePixels.Length-width-2; pixel++)
+                //{
+
+                //for (int i=0; i< targetPixelRegion.ImagePixels.Length-1; i++)
+                //{
+
+                //    //targetPixelRegion.ImagePixels[i];
+                //    //targetPixelRegion.ForEachRow((index, width, position) =>
+                //    //{
+
+                //    //    //Parallel.For(0, width, (i) =>
+                //    //    for (int i = 0; i < width; i++)
+                //    //    {
+
+                //    //array.Clear();
+                //    ////var pixel = index + i;
+                //    //array.Add(sourcePixelRegion.ImagePixels[pixel - width - 1]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel - width]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel - width + 1]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel - 1]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel + 1]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel + width - 1]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[ pixel + width]);
+                //    //array.Add(sourcePixelRegion.ImagePixels[pixel + width + 1]);
+
+                //    targetPixelRegion.ImagePixels[pixel] = sourcePixelRegion.ImagePixels[pixel - 1];// GetPixelOrDefault(sourcePixelRegion, pixel);// array[4];
+                //    }//  });
+                    //for (int i = 0; i < width; ++i)
+                    //{
+                    //    //Almacenamos los pixels alrededor del actual en un array ordenado para coger la mediana
+                    //    //var c = sourcePixelRegion.ImagePixels[index + i];
+                    //    List<uint> array = new List<uint>();
+
+                    //    var pixel = index + i;
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width - 1));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - width + 1));
+                                                                      
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel - 1));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + 1));
+                                                                      
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width - 1));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width));
+                    //    array.Add(GetPixelOrDefault(sourcePixelRegion, pixel + width + 1));
+                        
+                    //    targetPixelRegion.ImagePixels[pixel] = array.OrderBy(it=>it).ToList()[4];
+                    //}
+
+                //});
             }
 
             private uint GetPixelOrDefault(PixelRegion pixelRegion, int pixel)
             {
-                if(pixel<0 || pixel>pixelRegion.ImagePixels.Length)
+                if(pixel<0 || pixel>=pixelRegion.ImagePixels.Length)
                     return (uint)(0xff000000);
                 else
                     return pixelRegion.ImagePixels[pixel];                
+            }
+            private uint GetPixelOrDefault(uint[] image, int pixel, uint pixelDefault)
+            {
+                if (pixel < 0 || pixel >= image.Length)
+                    return pixelDefault;//(uint)(0xff000000);
+                else
+                    return image[pixel];
             }
         }
     }
